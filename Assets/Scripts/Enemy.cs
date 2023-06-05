bb07ede58c77022177fcb924cabdb458c3cbeb68
bryce-eldridge <bryce.eldridge121003@gmail.com>
2023-06-05 15:41:14 +1000

    Enemy Movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform targetDestination;
+    [SerializeField] float speed;
+
+    Rigidbody2D rgdbd2d;
+
+    private void Awake()
+    {
+        rgdbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (targetDestination.position - transform.position).normalized;
+        rgdbd2d.velocity = direction * speed;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Character>())
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attacking The Player");
+    }
+}
