25d37f033f44a9ea194aecc326253cad725a22a2
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-06-06 09:36:55 +1000

    health drops

---
+++ Assets/Scripts/DropOnDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropOnDestroy : MonoBehaviour
+{
+    [SerializeField] GameObject dropItemPickup;
+    [SerializeField] [Range(0f, 1f)] float chance = 1f;
+
+    private void OnDestroy()
+    {
+        if(Random.value < chance)
+        {
+            Transform t = Instantiate(dropItemPickup).transform;
+            t.position = transform.position;
+        }
+    }
+}
