f19b4f61c65ae09ddc28b5cb47e41165a51f10e3
bryce-eldridge <bryce.eldridge121003@gmail.com>
2023-06-06 19:15:53 +1000

    WeaponDataSmolUpdate

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] Transform weaponObjectContainer;
+
+    [SerializeField] WeaponData startingWeapon;
+
+    public void AddWeapon(WeaponData weaponData)
+    {
+        GameObject weaponGameObject = Instantiate(weaponData.weaponBasePrefab, weaponObjectContainer);
+    }
+}
