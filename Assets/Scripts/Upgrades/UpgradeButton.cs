be3393e00452ab77586f572ba967e62accefa2ee
RussKOH14 <101307699+RussKOH14@users.noreply.github.com>
2023-06-18 14:20:41 +1000

    Description for upgrades

--- Assets/Scripts/Upgrades/UpgradeButton.cs
+++ Assets/Scripts/Upgrades/UpgradeButton.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UpgradeButton : MonoBehaviour
 {
     [SerializeField] Image icon;
+    [SerializeField] TextMeshProUGUI descriptionText; // Use TextMeshProUGUI for TextMeshPro support
 
     public void Set(UpgradeData upgradeData)
     {
         icon.sprite = upgradeData.icon;
+        descriptionText.text = upgradeData.description; // Set the upgrade description text
     }
 
     internal void Clean()
     {
         icon.sprite = null;
+        descriptionText.text = string.Empty; // Clear the upgrade description text
     }
 }
