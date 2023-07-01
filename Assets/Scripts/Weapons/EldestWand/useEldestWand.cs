69f3c43dd33b88a085be8d6937fe7dbb7844f36a
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-07-01 15:48:25 +1000

    thats enough for today

--- Assets/Scripts/Weapons/EldestWand/useEldestWand.cs
+++ Assets/Scripts/Weapons/EldestWand/useEldestWand.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class useEldestWand : MonoBehaviour
 {
     public GameObject wand;
     public bool coolDownOver = true;
+    public float timeLeft = 10.0f;
+    public TextMeshProUGUI startText;
 
     public void UseWand()
     {
         if (coolDownOver)
         {
             wand.SetActive(true);
+            timeLeft = 10.0f;
         }
         
     }
 
+    private void Update()
+    {
+        startText.text = (timeLeft).ToString("0");
+        if (!coolDownOver)
+        {
+            timeLeft -= Time.deltaTime;
+
+            if (timeLeft < 0)
+            {
+                coolDownOver = true;
+
+            }
+        }
+
+    }
     public void StartTimer()
     {
         coolDownOver = false;
+        
     }
 }
