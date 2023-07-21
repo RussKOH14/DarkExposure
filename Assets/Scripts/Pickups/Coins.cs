6c2bfdd912e18b33a5869af23eb5d424adaedf80
Teresa Fares <107094799+Shado5@users.noreply.github.com>
2023-07-21 15:52:49 +1000

    Update Coins.cs

--- Assets/Scripts/Pickups/Coins.cs
+++ Assets/Scripts/Pickups/Coins.cs
@@ -9,25 +9,19 @@ public class Coins : MonoBehaviour
 
     private void Awake()
     {
+        PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
         int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
         coinsCountText.text = coinsAquired.ToString();
         
     }
     private void Update()
     {
-        PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
-        PlayerPrefs.Save();
         int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
         coinsCountText.text = coinsAquired.ToString();
-        if (dataContainer.coins < 0)
-        {
-            dataContainer.coins = 0;
-        }
     }
     public void Add (int count)
     {
         int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
-        coinsCountText.text = coinsAquired.ToString();
         dataContainer.coins += count;
         PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
         PlayerPrefs.Save();
