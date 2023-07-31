b932616499eed59ada14b33da4b4a836c5a072a3
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-07-31 17:52:54 +1000

    SHOP WORKSSSSS

---
+++ Assets/Scripts/Upgrades/ShopGameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopGameManager : MonoBehaviour
+{
+ 
+    public static ShopGameManager Instance;
+
+
+    public DataContainer dataContainer;
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+        LoadData();
+    }
+
+
+    public void LoadData()
+    {
+        dataContainer.coins = PlayerPrefs.GetInt("coinsAquired", dataContainer.coins);
+        dataContainer.addedHealth = PlayerPrefs.GetInt("addedHealth", dataContainer.addedHealth);
+        dataContainer.addedDamage = PlayerPrefs.GetInt("addedDamage", dataContainer.addedDamage);
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
+        PlayerPrefs.SetInt("addedHealth", dataContainer.addedHealth);
+        PlayerPrefs.SetInt("addedDamage", dataContainer.addedDamage);
+        PlayerPrefs.Save();
+    }
+}
--- Assets/Scripts/Upgrades/ShopManager.cs
+++ Assets/Scripts/Upgrades/ShopManager.cs
@@ -1,71 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
-    [SerializeField] DataContainer dataContainer;
-    [SerializeField] TMPro.TextMeshProUGUI coins;
+    [SerializeField] TMPro.TextMeshProUGUI coinsText;
     public int healthCost;
     public int damageCost;
-    
+
     private void Awake()
     {
-        PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
-        PlayerPrefs.Save();
-        int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
-        coins.text = coinsAquired.ToString();
 
-        PlayerPrefs.SetInt("addedHealth",dataContainer.addedHealth);
-        PlayerPrefs.SetInt("addedDamage",dataContainer.addedDamage);
-    }
-    private void Update()
-    {
-        PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
-        PlayerPrefs.Save();
-        int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
-        coins.text = coinsAquired.ToString(); 
+        LoadData();
+        UpdateCoinsText();
     }
 
     public void Health()
     {
-        if (dataContainer.coins >= healthCost)
+        if (ShopGameManager.Instance.dataContainer.coins >= healthCost)
         {
-            int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
-            
-            dataContainer.coins -=healthCost;
-            PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
-            PlayerPrefs.Save();
-            coins.text = coinsAquired.ToString();
-
-            PlayerPrefs.GetInt("addedHealth");
-            dataContainer.addedHealth += 100;
-            PlayerPrefs.SetInt("addedHealth", dataContainer.addedHealth);
-            PlayerPrefs.Save();
-
+            ShopGameManager.Instance.dataContainer.coins -= healthCost;
+            ShopGameManager.Instance.dataContainer.addedHealth += 100;
+            SaveData();
+            UpdateCoinsText();
         }
-        
     }
+
     public void Damage()
     {
-        
-        if (dataContainer.coins >= damageCost)
+        if (ShopGameManager.Instance.dataContainer.coins >= damageCost)
         {
-            Debug.Log("worked");
-            int coinsAquired = PlayerPrefs.GetInt("coinsAquired");
-            
-            dataContainer.coins -=damageCost;
-            PlayerPrefs.SetInt("coinsAquired", dataContainer.coins);
-            PlayerPrefs.Save();
-            coins.text = coinsAquired.ToString();
-
-            PlayerPrefs.GetInt("addedDamage");
-            dataContainer.addedDamage += 100;
-            PlayerPrefs.SetInt("addedDamage", dataContainer.addedDamage);
-            PlayerPrefs.Save();
-
+            ShopGameManager.Instance.dataContainer.coins -= damageCost;
+            ShopGameManager.Instance.dataContainer.addedDamage += 100;
+            SaveData();
+            UpdateCoinsText();
         }
-        
+    }
+
+    private void UpdateCoinsText()
+    {
+        coinsText.text = ShopGameManager.Instance.dataContainer.coins.ToString();
+    }
+
+
+    private void LoadData()
+    {
+        ShopGameManager.Instance.LoadData();
+    }
+
+    private void SaveData()
+    {
+        ShopGameManager.Instance.SaveData();
     }
 }
