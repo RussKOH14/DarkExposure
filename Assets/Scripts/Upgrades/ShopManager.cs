98eff619ebbc1a5cb0a97d683f28e16286b947c7
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-07-31 18:23:58 +1000

    Revert "reset button"
    This reverts commit 262c77feed53cd9e3369f01e2a2ef88331dd335e.

--- Assets/Scripts/Upgrades/ShopManager.cs
+++ Assets/Scripts/Upgrades/ShopManager.cs
@@ -3,8 +3,7 @@ using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
-    public DataContainer dataContainer;
-    [SerializeField] TextMeshProUGUI coinsText;
+    [SerializeField] TMPro.TextMeshProUGUI coinsText;
     public int healthCost;
     public int damageCost;
 
@@ -40,15 +39,6 @@ public class ShopManager : MonoBehaviour
     private void UpdateCoinsText()
     {
         coinsText.text = ShopGameManager.Instance.dataContainer.coins.ToString();
-    }
-
-    public void Reset()
-    {
-        ShopGameManager.Instance.dataContainer.coins = 0;
-        ShopGameManager.Instance.dataContainer.addedHealth = 0;
-        ShopGameManager.Instance.dataContainer.addedDamage = 0;
-        SaveData();
-        UpdateCoinsText();
     }
 
 
