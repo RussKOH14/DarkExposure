0384a7d26e33e6b850eb3c92f74b3939e6d8f191
Shado5 <107094799+Shado5@users.noreply.github.com>
2023-08-07 13:32:32 +1000

    click to skip text

--- Assets/Scripts/Misc_/SceneSwitcher.cs
+++ Assets/Scripts/Misc_/SceneSwitcher.cs
@@ -3,10 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    [SerializeField] private string sceneName = "NextScene"; // Name of the scene to switch to
-    [SerializeField] private float delay = 5f; // Time delay before switching scenes
-
-    private float timer = 0f; // Timer to keep track of the elapsed time
+    [SerializeField] private string sceneName = "NextScene";
 
     public GameObject spaceToSkipUI;
     private bool canSkip;
@@ -18,18 +15,6 @@ public class SceneSwitcher : MonoBehaviour
     }
     private void Update()
     {
-        // Increase the timer based on the time elapsed since the last frame
-        timer += Time.deltaTime;
-
-        // Check if the specified delay has passed
-        if (timer >= delay)
-        {
-            // Reset the timer
-            timer = 0f;
-
-            // Switch to the specified scene
-            SceneManager.LoadScene(sceneName);
-        }
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
--- Assets/Scripts/UI/TypewriterEffect.cs
+++ Assets/Scripts/UI/TypewriterEffect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TypewriterEffect : MonoBehaviour
 {
@@ -13,42 +14,72 @@ public class TypewriterEffect : MonoBehaviour
     private int charCounter = 0;
     public AudioClip typeSound;
     public AudioSource audioSource;
+    private bool isTyping = true;
 
-   
+    public string sceneName;
 
     void Start()
     {
-        
         tmpText = GetComponent<TMP_Text>();
         StartCoroutine(ShowText());
         audioSource = gameObject.AddComponent<AudioSource>();
     }
 
-    IEnumerator ShowText()
+    void Update()
     {
-        while (currentLayer < fullTexts.Length)
+        if (Input.GetMouseButtonDown(0))
         {
-            charCounter = 0;
-
-            while (charCounter < fullTexts[currentLayer].Length)
+            if (isTyping)
             {
-                currentText = fullTexts[currentLayer].Substring(0, charCounter + 1);
-                tmpText.text = currentText;
-
-                
-                if (charCounter % 2 == 0)
-                {
-                    audioSource.PlayOneShot(typeSound);
-                }
-
-                charCounter++;
-
-                yield return new WaitForSeconds(delay);
+                StopCoroutine(ShowText());
+                tmpText.text = fullTexts[currentLayer];
+                isTyping = false;
             }
-            yield return new WaitForSeconds(3f);
-            currentText = "";
-            tmpText.text = currentText;
-            currentLayer++;
+            else
+            {
+                currentLayer++;
+
+                if (currentLayer < fullTexts.Length)
+                {
+                    tmpText.text = "";
+                    isTyping = true;
+                    DisplayText();
+                }
+                else
+                {
+                    SceneManager.LoadScene(sceneName);
+                }
+            }
         }
     }
+
+    void DisplayText()
+    {
+        if (isTyping)
+        {
+            StartCoroutine(ShowText());
+        }
+    }
+
+    IEnumerator ShowText()
+    {
+        charCounter = 0;
+        while (charCounter < fullTexts[currentLayer].Length && isTyping)
+        {
+            currentText = fullTexts[currentLayer].Substring(0, charCounter + 1);
+            tmpText.text = currentText;
+
+            if (charCounter % 2 == 0)
+            {
+                audioSource.PlayOneShot(typeSound);
+            }
+
+            charCounter++;
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        isTyping = false;
+    }
 }
+
